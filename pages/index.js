9d53d80c5ad3f93e1c6e0a4601efdf31275f3903
Gov <govinda.escobar@gmail.com>
2022-05-24 20:53:08 -0700

    edited bio

--- pages/index.js
+++ pages/index.js
@@ -23,8 +23,8 @@ export default function Home({ allPostsData }) {
       <section className={utilStyles.headingMd}>
         <p>Physicist, gone writer, gone software engineer.</p>
         <p>
-          (This sample site is the result of completing the <a href="https://nextjs.org/learn/basics/create-nextjs-app">Create a Next.js App tutorial</a>.
-          It concerns frontend development concepts and the basics of Nextjs.)
+          This sample site is the result of completing the <a href="https://nextjs.org/learn/basics/create-nextjs-app">Create a Next.js App tutorial</a>.
+          It concerns frontend development concepts and the basics of Nextjs.
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
